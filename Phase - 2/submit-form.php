281df98d1d90afbeb314e1cd4afb9399ff6b3467
zainab028 <158202733+zainab028@users.noreply.github.com>
2024-05-19 14:17:01 +0530

    Delete Phase - 2/submit-form.php

--- Phase - 2/submit-form.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // Here,
