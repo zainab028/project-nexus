717e573e57cbb9ff3bd413f78f00a665fec71340
zainab028 <158202733+zainab028@users.noreply.github.com>
2024-05-19 14:14:39 +0530

    Add files via upload

---
+++ Phase - 2/submit-form.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Here,
